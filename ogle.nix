b57acf4d3ccc52b12c5de222cd74091a44abdf0d
Leandro Lisboa Penz <lpenz@lpenz.org>
2021-02-01 21:23:15 +0000

    Upgrade ogle to 1.3.1

--- ogle.nix
+++ ogle.nix
@@ -2,14 +2,14 @@
 
 rustPlatform.buildRustPackage rec {
   name = "ogle-${version}";
-  version = "1.3.0";
+  version = "1.3.1";
 
   src = fetchFromGitHub {
     owner = "lpenz";
     repo = "ogle";
     rev = "${version}";
-    sha256 = "sha256:0jfw96d7w6zqjs5bd2706yw7xi7c74agkmy320hr5ygyb9qhs372";
+    sha256 = "sha256:0lwv4d683dzc8gc70ms0532rs17fxy87a5xcs0v9bq7ghypazcjv";
   };
 
-  cargoSha256 = "sha256:0glic4z12ahajps51z7rsslgk6d7zahjnci1sg38cwjypxcz7yc6";
+  cargoSha256 = "sha256:0w1kx4h9qmbj9jy619q9bf1fhka31mh0w46k5i96b9bqfwk9qhii";
 }
